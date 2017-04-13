b344409869bb5a2963e8a3b49b370d725d75d4e5
londondash <moddedsincebirth@gmail.com>
2017-04-12 20:11:55 -0400

    Add files via upload

--- masquerade.sh
+++ masquerade.sh
@@ -18,7 +18,7 @@ W E L C O M E  T O  T H E                                  '||
 .|| || ||.'|..'|'|'..|''|..'||'|..'|.'|...'.||.  '|..'|'|..'||.'|...' 
                             ||                                  
                            ''''\033[m"
-	sleep 
+	sleep 2
 installutilitiesandcoms
     else
     clear
@@ -47,8 +47,6 @@ installutilitiesandcoms
 }
 
 function retrievefilesfromgit() {
-	
-	
 if [ -d /tmp/masquerade ] ; then
 	echo -e "\e[0m(òÓ,)_\,,/\e[32m resetting install path\033[m"
 	rm -rf /tmp/masquerade
@@ -69,7 +67,7 @@ U T I L I T Y  S O F T W A R E  I N S T A L L A T I O N
 \e[1;37mthe following utility software packages will be installed;
    
     \e[1;36m#etcher #neofetch #crashplan #safecopy #grub-customizer #gksu
-    #testdisk #veracrypt #bleachbit #catfish #messengerfordesktop-bin
+    #testdisk #veracrypt #bleachbit #catfish #messengerfordesktop-bin #ntp
 	\033[m"
 
 read -p $'\033[1;37mdo you want to install the utility software? '"$notexpanded"'(y/N) ' prompt
@@ -82,10 +80,10 @@ yaourt crashplan
 yaourt safecopy
 yaourt grub-customizer
 yaourt messengerfordesktop-bin
-sudo pacman -S testdisk veracrypt bleachbit catfish gksu
+sudo pacman -S testdisk veracrypt bleachbit catfish gksu ntp
 installmediasoftware
 else
-echo -e "\e[0m┌∩┐(✖╭╮✖)┌∩┐\e[91mskipping utility bundle installation\033[m"
+echo -e "\e[0m┌∩┐(✖╭╮✖)┌∩┐\e[91m skipping utility bundle installation\033[m"
 installmediasoftware
 fi
 }
@@ -107,7 +105,7 @@ echo -e "\e[0m(òÓ,)_\,,/\e[32m installing media software\033[m"
 sudo pacman -S lmms ardour qjackctl jack2 ladspa audacity blender clementine kodi k3b recordmydesktop vlc
 installgamesoftware
 else
-echo -e "\e[0m┌∩┐(✖╭╮✖)┌∩┐\e[91mskipping media bundle installation\033[m"
+echo -e "\e[0m┌∩┐(✖╭╮✖)┌∩┐\e[91m skipping media bundle installation\033[m"
 installgamesoftware
 fi
 }
@@ -125,13 +123,12 @@ G A M E  S O F T W A R E  I N S T A L L A T I O N
 read -p $'\033[1;37mdo you want to install the game software? '"$notexpanded"'(y/N) ' prompt
 if [[ $prompt =~ [yY](es)* ]]
 then
-echo -e "\e[0m(òÓ,)_\,,/\e[32m installing game software\033[m"
-sudo pacman -S astromenace chromium-bsu critter gweled gnome-hearts neverball pysolfc supertuxkart
-	
-installhackingoftware	
+	echo -e "\e[0m(òÓ,)_\,,/\e[32m installing game software\033[m"
+	sudo pacman -S astromenace chromium-bsu critter gweled gnome-hearts neverball pysolfc supertuxkart
+	installhackingoftware	
 else
-echo -e "\e[0m┌∩┐(✖╭╮✖)┌∩┐\e[91mskipping game bundle installation\033[m"
-installhackingoftware
+	echo -e "\e[0m┌∩┐(✖╭╮✖)┌∩┐\e[91m skipping game bundle installation\033[m"
+	installhackingoftware
 fi
 }
 
@@ -148,82 +145,96 @@ H A C K I N G  S O F T W A R E  I N S T A L L A T I O N
 read -p $'\033[1;37mdo you want to install the hacking software? '"$notexpanded"'(y/N) ' prompt
 if [[ $prompt =~ [yY](es)* ]]
 then
+
+cd /tmp/masquerade/
+	echo -e "\e[0m(òÓ,)_\,,/\e[32m installing hacking repository\033[m"
+	curl -O https://blackarch.org/strap.sh
+	sudo bash strap.sh
+	yaourt -Syu
+
 echo -e "\e[0m(òÓ,)_\,,/\e[32m installing hacking software\033[m"
-
-	
-installopenboxthemes	
+#sudo pacman -S blackarch-anti-forensic blackarch-menus blackarch-automation blackarch-backdoor blackarch-binary blackarch-bluetooth blackarch-code-audit blackarch-cracker blackarch-crypto blackarch-database blackarch-debugger blackarch-decompiler blackarch-defensive blackarch-disassembler blackarch-dos
+#blackarch-drone blackarch-exploitation
+#sudo pacman -S blackarch-crypto --noconfirm
+#sudo pacman -S blackarch-fingerprint --noconfirm
+#sudo pacman -S blackarch-firmware --noconfirm
+#sudo pacman -S blackarch-forensic --noconfirm
+#sudo pacman -S blackarch-fuzzer --noconfirm
+#sudo pacman -S blackarch-hardware --noconfirm
+#sudo pacman -S blackarch-honeypot --noconfirm
+#sudo pacman -S blackarch-keylogger --noconfirm
+#sudo pacman -S blackarch-malware --noconfirm
+#udo pacman -S blackarch-misc --noconfirm
+#sudo pacman -S blackarch-mobile --noconfirm
+#sudo pacman -S blackarch-networking --noconfirm
+#sudo pacman -S blackarch-nfc --noconfirm
+#sudo pacman -S blackarch-packer --noconfirm
+#sudo pacman -S blackarch-proxy --noconfirm
+#sudo pacman -S blackarch-recon --noconfirm
+#sudo pacman -S blackarch-reversing --noconfirm
+#sudo pacman -S blackarch-scanner --noconfirm
+#sudo pacman -S blackarch-sniffer --noconfirm
+#sudo pacman -S blackarch-socia --noconfirm
+#sudo pacman -S blackarch-spoof --noconfirm
+#sudo pacman -S blackarch-threat-model --noconfirm
+#sudo pacman -S blackarch-tunnel --noconfirm
+#sudo pacman -S blackarch-unpacker --noconfirm
+#sudo pacman -S blackarch-voip --noconfirm
+#sudo pacman -S blackarch-webapp --noconfirm
+#sudo pacman -S blackarch-windows --noconfirm
+#sudo pacman -S blackarch-wireless --noconfirm
+configurearchlinux	
 else
-echo -e "\e[0m┌∩┐(✖╭╮✖)┌∩┐\e[91mskipping hacking bundle installation\033[m"
-installopenboxthemes
+echo -e "\e[0m┌∩┐(✖╭╮✖)┌∩┐\e[91m skipping hacking bundle installation\033[m"
+configurearchlinux
 fi
 }
 
-function installopenboxthemes {
-echo -e "\e[1;36m
-O P E N B O X  T H E M E  I N S T A L L A T I O N
 
-\e[1;37mthe following openbox themes will be installed;
-   
-    \e[1;36m#flatstudiodark #win10-colors #laza_dark #mythos #ygoreus
-    #bproject #dyna_black #nightmare 
-	\033[m"
-
-read -p $'\033[1;37mdo you want to install the openbox themes? '"$notexpanded"'(y/N) ' prompt
-if [[ $prompt =~ [yY](es)* ]]
-then
-echo -e "\e[0m(òÓ,)_\,,/\e[32m installing openbox themes\033[m"
-#
-configurearchlinux
-else
-echo -e "\e[0m┌∩┐(✖╭╮✖)┌∩┐\e[91mskipping openbox themes installation\033[m"
-configurearchlinux
-fi
-}
 
 function configurearchlinux {
 echo -e "\e[1;36m
 C O N F I G U R I N G  T H E  S Y S T E M\033[m"
 retrievefilesfromgit
-
-echo -e "\e[0m(òÓ,)_\,,/\e[32mi not afraid welcome to the masquerade\033[m"
-sleep 5
+sleep 2
 
 sudo sh /tmp/masquerade/masqueraderoot.sh
 
-echo -e "\e[0m(òÓ,)_\,,/\e[32mconfiguring openbox\033[m"
+echo -e "\e[0m(òÓ,)_\,,/\e[32m configuring openbox\033[m"
 	#mv ~/.config/openbox/autostart ~/.config/openbox/autostart.BU -i
 	#mv /tmp/masquerade/autostart ~/.config//openbox/autostart
 	
 	#mv ~/.config/openbox/menu.xml ~/.config/openbox/menu.xml.BU -i
 	#mv /tmp/masquerade/menu.xml ~/.config//openbox/menu.xml
 	
-echo -e "\e[0m(òÓ,)_\,,/\e[32mconfiguring conky\033[m"
+	#mv ~/.config/openbox/rc.xml ~/.config/openbox/rc.xml.BU -i
+	#mv /tmp/masquerade/rc.xml ~/.config//openbox/rc.xml
+	
+echo -e "\e[0m(òÓ,)_\,,/\e[32m configuring conky\033[m"
 	#mv ~/.config/conky.conf ~/.config/conky.conf.BU -i
 	#mv /tmp/masquerade/conky.conf ~/.config/conky.conf
 
-echo -e "\e[0m(òÓ,)_\,,/\e[32mconfiguring tint2\033[m"
+echo -e "\e[0m(òÓ,)_\,,/\e[32m configuring tint2\033[m"
 	#mv ~/.config/tint2/tint2rc ~/.config/tint2/tint2rc.BU -i
 	#mv /tmp/masquerade/tint2rc ~/.config/tint2/tint2rc
 
 
-echo -e "\e[0m(òÓ,)_\,,/\e[32mconfiguring bashrc\033[m"
+echo -e "\e[0m(òÓ,)_\,,/\e[32m configuring bashrc\033[m"
 	#mv ~/.bashrc ~/bashrc.BU -i
 	#mv /tmp/masquerade/bashrc ~/.bashrc
 
-echo -e "\e[0m(òÓ,)_\,,/\e[32mconfiguring plank\033[m"
+echo -e "\e[0m(òÓ,)_\,,/\e[32m configuring plank\033[m"
 	#mv ~/.config/plank/dock1/settings ~/.config/plank/dock1/settings.BU -i
 	#mv /tmp/masquerade/settings ~/.config/plank/dock1/settings
 
 
 
-echo -e "\e[0m(òÓ,)_\,,/\e[32mconfiguring light-lock\033[m"
-echo -e "\e[0m(òÓ,)_\,,/\e[32mconfiguring grub\033[m"
+echo -e "\e[0m(òÓ,)_\,,/\e[32m configuring light-lock\033[m"
+echo -e "\e[0m(òÓ,)_\,,/\e[32m configuring grub\033[m"
 
 	#mv /boot/grub/themes/Antergos-Default/background.png /boot/grub/themes/Antergos-Default/background.original.png -i
 	#mv /tmp/masquerade/grubbackground.png /boot/grub/themes/Antergos-Default/background.png
 
-
-echo -e "\e[0m(òÓ,)_\,,/\e[32mconfiguring gtk\033[m"
 
 systemcleanup
 }
@@ -232,9 +243,8 @@ function systemcleanup() {
 echo -e "\e[1;36m
 C L E A N I N G  T H E  S Y S T E M\033[m"
 
-echo -e "\e[0m(òÓ,)_\,,/\e[32mconfiguring your system\033[m"
+echo -e "\e[0m(òÓ,)_\,,/\e[32m configuring your system\033[m"
 }
 
 
-configurearchlinux
-#checkprivileges
+ checkprivileges
